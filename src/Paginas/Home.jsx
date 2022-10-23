c6e68f96de7645d0ba33845353c05e98ccc144c6
Yeraf <yerafarceguerrero9@gmail.com>
2022-10-23 16:42:13 -0600

    commit

--- src/Paginas/Home.jsx
+++ src/Paginas/Home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import '../Css/Cssapp/Home.css';
-import Logo from '../Img/Logos/llama2.png';
+import "../Css/Cssapp/Home.css";
+import Logo from "../Img/Logos/llama2.png";
 
 function Home() {
   return (
     <div className="div-home-complete">
-    <div className="container div-home-title">
-      <img className="home-logo-img" src={Logo}></img>Yoku Technology
-      <img className="home-logo-img-rotate" src={Logo}></img>
-    </div>
+      <div className="home-img-container">
+        <div className="container div-home-title">
+          {/* <img className="home-logo-img" src={Logo}></img>Yoku Technology
+        <img className="home-logo-img-rotate" src={Logo}></img> */}
+        </div>
+        <article className="container article-home-intro">
+          <h3>Yoku Technology le ofrece</h3>
+          <ul>
+            <li>Reparación y mantenimiento de computadoras.</li>
+            <li>Venta de hardware para su PC o laptop.</li>
+            <li>Instalación de cámaras circuito cerrado.</li>
+            <li>Creación de páginas Web.</li>
+            <li>Entre otros servicios.</li>
+          </ul>
+        </article>
+      </div>
     </div>
   );
 }
