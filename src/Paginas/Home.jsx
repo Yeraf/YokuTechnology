57e330253a68d46a0c45d2ed25fa55123aa03985
Yeraf <yerafarceguerrero9@gmail.com>
2022-10-22 18:35:36 -0600

    commit

--- src/Paginas/Home.jsx
+++ src/Paginas/Home.jsx
@@ -4,9 +4,11 @@ import Logo from '../Img/Logos/llama2.png';
 
 function Home() {
   return (
+    <div className="div-home-complete">
     <div className="container div-home-title">
       <img className="home-logo-img" src={Logo}></img>Yoku Technology
       <img className="home-logo-img-rotate" src={Logo}></img>
+    </div>
     </div>
   );
 }
