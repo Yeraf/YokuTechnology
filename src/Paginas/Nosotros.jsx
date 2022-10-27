7a651d95493574e938209fac543cfbf0d7f47ab0
Yeraf <yerafarceguerrero9@gmail.com>
2022-10-26 22:20:44 -0600

    nosotros

--- src/Paginas/Nosotros.jsx
+++ src/Paginas/Nosotros.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import '../Css/Cssapp/Nosotros.css';
+import Logo from '../Img/Logos/llama3.png';
 
 function Nosotros() {
   return (
     <div className="container nosotros-jumbotron">
+      <hr></hr>
       <div className="jumbotron bg-warning text-black">
         <h1 class="display-4 text-black"><b>Yoku Technology</b></h1>
         <p className="lead text-black">
         Somos una empresa de tecnología en la cual puedes encontrar servicios como reparación de computadoras, formateo, mantenimiento, instalación de cámaras, creación de paginas Web, implementación de red y cableado entre otros servicios técnicos.
         </p>
+        <img src={Logo} className="icons-general-nosotros"></img>
+        <img src={Logo} className="icons-general-rotate-nosotros"></img>
+        <img src={Logo} className="icons-general-rotate-nosotros"></img>
         <hr class="my-4"></hr>
         <p>
           It uses utility classes for typography and spacing to space content
           out within the larger container.
         </p>
       </div>
+      <hr></hr>
     </div>
   );
 }
