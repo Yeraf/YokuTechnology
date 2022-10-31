35daf60789bda975e654d062c5a5472d4ae4d6c6
Yeraf <yerafarceguerrero9@gmail.com>
2022-10-31 16:21:55 -0600

    email commit

--- src/CompStatics/EnviarEmail.jsx
+++ src/CompStatics/EnviarEmail.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function EnviarEmail() {
   return (
@@ -6,4 +6,4 @@ function EnviarEmail() {
   )
 }
 
-export default EnviarEmail
+export default EnviarEmail;
--- src/Paginas/Contacto.jsx
+++ src/Paginas/Contacto.jsx
@@ -1,46 +1,67 @@
 import React from "react";
 import "../Css/Cssapp/Contacto.css";
+import emailjs from "@emailjs/browser";
 
 function Contacto() {
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_3cg1f6p",
+        "template_l5hples",
+        event.target,
+        "FoFts6LOyy45_0Jmz"
+      )
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+    alert("Correo enviado con éxito");
+  };
 
   return (
     <div className="container mx-auto form-contacto-complete">
-      <form className="form-contacto" id="form">
+      <form className="form-contacto" id="form" onSubmit={sendEmail}>
         <h2 className="h2-contacto">Contacto</h2>
         <input
           className="input-contacto"
           type="text"
-          name="name"
+          name="user_name"
           id="name"
-          placeholder="Nombre" required
+          placeholder="Nombre"
+          required
         ></input>
         <input
           className="input-contacto"
           type="text"
-          name="email"
+          name="user_email"
           id="email"
-          placeholder="Correo" required
+          placeholder="Correo"
+          required
         ></input>
         <input
           className="input-contacto"
           type="number"
-          name="phone"
+          name="user_phone"
           id="phone"
-          placeholder="Teléfono" required
+          placeholder="Teléfono"
+          required
         ></input>
         <textarea
           className="input-contacto textarea"
           type="text"
-          name="message"
+          name="user_message"
           id="message"
           placeholder="Mensaje"
         ></textarea>
-        <input className="btn btn-info input-contacto-buttom" type="submit" id="button" value="ENVIAR MENSAJE" ></input>
-      </form>  
+        <input
+          className="btn btn-info input-contacto-buttom"
+          type="submit"
+          id="button"
+          value="ENVIAR MENSAJE"
+        ></input>
+      </form>
     </div>
   );
 }
 
 export default Contacto;
-
-
